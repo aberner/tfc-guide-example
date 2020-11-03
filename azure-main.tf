88c8ecd8c4d56990469b8109af24f04f85f7373e
Arne Berner <arne.berner@bouvet.no>
2020-11-03 10:52:29 +0100

    Update azure-main.tf

--- azure-main.tf
+++ azure-main.tf
@@ -1,14 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.34"
-    }
-  }
-}
 
 #Configure the Azure Provider
 provider "azurerm" { 
+    version = ">= 2.33"
     features {}
 }
 
